024ba7b43d794d8fa1baec26be594c88e7931a75
Martin <holstm75@gmail.com>
2025-03-24 11:44:20 +0100

    Ting

--- javascript.js
+++ javascript.js
@@ -1,29 +1,73 @@
+var knapoverskrift = document.getElementById('leggtiloverskrift')
+var knaptekst = document.getElementById('leggetiltekst')
+var knapfarge = document.getElementById('bytterfarge')
+var knaptekstfarge = document.getElementById('endretekstfarge')
+var knapflytttekstvenstre = document.getElementById('flytttekstenvenstre')
+var knapflyttteksthøyre = document.getElementById('flytttekstenhøyre')
+var knapliste = document.getElementById('leggtilliste')
+var knapbilde = document.getElementById('leggtilbilde')
+
+knapoverskrift.addEventListener('click', leggtiloverskrift)
+knaptekst.addEventListener('click', leggetiltekst)
+knapfarge.addEventListener('click', bytterfarge)
+knaptekstfarge.addEventListener('click', endretekstfarge)
+knapflytttekstvenstre.addEventListener('click', flytttekstenvenstre)
+knapflyttteksthøyre.addEventListener('click', flytttekstenhøyre)
+knapliste.addEventListener('click', leggtilliste)
+knapbilde.addEventListener('click', leggtilbilde)
 
 
-
-function add() {
-    var farge = document.getElementById('input').value
+/** endrer bakgrunsfargen på nettsiden*/
+function bytterfarge() {
+    var farge = document.getElementById('fargeinn').value
     if (farge === "Martin") {
         farger = ['red', 'blue', 'green']
         farge = farger[Math.floor(Math.random() * farger.length)]
     }
-    document.getElementById('body').style.backgroundColor = farge
+    document.getElementById('høyre').style.backgroundColor = farge
 }
-function tekst() {
+/** Legger til tekst på netsiden*/
+function leggetiltekst() {
     var tekst = document.getElementById('tekstinn').value
     document.getElementById('fintekst').innerText
     = tekst
 }
-function overskrift() {
+/** Legger til en overskrift på netsiden*/
+function leggtiloverskrift() {
     var overskrift = document.getElementById('overskriftinn').value
     document.getElementById('overskrift').innerText
     = overskrift
 }
-function tekstfarge() {
+/** endrer teksten sin farge som du la til*/
+function endretekstfarge() {
     var tekstfarge = document.getElementById('tekstfarge').value
     document.getElementById('fintekst').style.color = tekstfarge
 }
-function liste() {
-    var listeting = document.getElementById('listeinn').value
-    document.getElementById('minliste').innerHTML += ('<li>'+ listeting + '</li>')
+/**Flytter teksten til siden*/
+var flytttekst = 10
+function flytttekstenhøyre(){
+    flytttekst = flytttekst + 10
+    document.getElementById('fintekst').style.paddingLeft = flytttekst + 'px'
+    document.getElementById('overskrift').style.paddingLeft = flytttekst + 'px' 
+    document.getElementById('minliste').style.paddingLeft = flytttekst + 'px'
+}
+function flytttekstenvenstre(){
+    flytttekst = flytttekst - 10
+    document.getElementById('fintekst').style.paddingLeft = flytttekst + 'px'
+    document.getElementById('fintekst').style.paddingLeft = flytttekst + 'px'
+    document.getElementById('overskrift').style.paddingLeft = flytttekst + 'px' 
+    document.getElementById('minliste').style.paddingLeft = flytttekst + 'px'
+}
+
+/** legger til en liste på nettsiden*/
+function leggtilliste() {
+    var liste = document.getElementById('listeinn').value
+    
+    document.getElementById('minliste').innerHTML += ('<li>'+ liste + '</li>')
     }   
+    /** legger til et random bilde på netsiden velger mellom de ulike bildene*/
+function leggtilbilde() {
+    bilder = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11','12', 'favurittbilde']
+    randombildeut = bilder[Math.floor(Math.random() * bilder.length)]
+    document.getElementById('bilde').innerHTML += ('<img id=bilder src="bilder/'+ randombildeut +'.jpg" alt="fint bilde">')
+}
